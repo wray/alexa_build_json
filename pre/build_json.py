40a38f45ee22c4e45c988f43d8fbfa2fe48cfe54
wray <wray@wrayesian.com>
2017-09-02 14:31:55 -0400

    Update build_json.py

--- pre/build_json.py
+++ pre/build_json.py
@@ -1,35 +1,24 @@
-import urllib2
 import json
 
 responses = {}
 
-for intent in ['toprepos','toplanguages','toporgs','topusers','topusersbylang','launch']:
+for intent in ['yourfirstintent','yoursecondintent']:
     responses[intent] = "<speak>Not yet implemented.</speak>"
 
-# { repo_name : ( isOrg, owner_name, lang ), ... }
-# [ (repo_name, star_fork_cnt), ... ]
-    
-# Top Repos
-# repos stars + forks
-# ordered list
-
-# Top Languages
-# repos stars + forks, unique by lang
+#
+# Put your custom code here. Call your APIs, scrape a website, ingest an RSS Feed, use your database.
+# Arrange your data to match the JSON convention:
+# responses = { 'intentname' : '<Your Response SSML>', 'intentwithslots' : { 'custom_slot_1' : '<Your response SSML>',
+#                                                                               'custom_slot_2' : '<Your response SSML'> }}
 # 
+#
+  
+# Keep the required responses below to ensure you skill reacts to launch/open, help, and end (as required by ASK).
 
-# Top Orgs
-# repos star + fork by org
-
-# Top Users
-# repos star + fork by user
-
-# Top Users by language
-# repos star + fork by lang
-    
-
-responses['launch'] = "<speack>Welcome to Virginia Coders. This skill uses the GitHub API to find the top repos, languages, organizations and users in Virginia. Try it out by asking, what are the top orgs.</speak> "
-responses['help'] = "<speak>Use Virginia Coders to check out the top repos, programming languages, organizations and users in Virginia according to GitHub. Try asking for the top programming languages used in Virginia; simply say, what are the top languages.</speak>"
-responses['end'] = "<speak>Thanks for checking out V A Coders. Talk to you soon.</speak>"
-    
-with open('responses/github.json', 'w') as outfile:
+responses['launch'] = "<speak>Welcome to my real cool skill. It gets data from cool places.</speak> "
+responses['help'] = "<speak>Use my cool skill that simplifies data retrieval for you. Suggest some intents to try.</speak>"
+responses['end'] = "<speak>Thanks for my cool skill. Talk to you soon.</speak>"
+   
+# Don't Forget, if you update your json filename, update it here too!
+with open('responses/response.json', 'w') as outfile:
     json.dump(responses, outfile)
